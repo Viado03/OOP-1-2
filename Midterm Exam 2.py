d993c4928beeef55c2fa6ae23d701c30144536ac
Viado03 <92914473+Viado03@users.noreply.github.com>
2022-04-28 14:56:43 +0800

    Add files via upload

---
+++ Midterm Exam 2.py
@@ -0,0 +1,11 @@
+from tkinter import *
+window = Tk()
+
+window.title("Special Midterm Exam OOP")
+window.geometry("500x400+20+10")
+
+btn = Button(window, text="Click To Change Color", fg ="black", activebackground= "Yellow")
+btn.place(x=190, y=200)
+
+
+window.mainloop()
